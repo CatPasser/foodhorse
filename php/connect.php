5744e8f5a9ae3fa5d3c9b89cdce26ce68fa8306f
陳宜文 <gyes03211@gmail.com>
2023-06-10 21:08:25 +0800

    1

--- php/connect.php
+++ php/connect.php
@@ -1,8 +1,8 @@
 <?php
-$serverhost = "127.0.0.1";
-$user = "master";
-$password = "HsaOPpRkj3c6Hqqp";
-$database = "foodhorse";
+$serverhost = "";
+$user = "";
+$password = "";
+$database = "";
 
 $connectNow = new mysqli($serverhost, $user, $password, $database);
 mysqli_query($connectNow,"SET NAMES 'UTF8'");
