1c310600d44c7dc34490f43d38878751121caa21
陳宜文 <gyes03211@gmail.com>
2023-06-10 20:59:28 +0800

    1

--- dashboard/login_php.php
+++ dashboard/login_php.php
@@ -6,12 +6,15 @@
 	
 ///////////////////
 	
-	require("conn_mysql.php");
-	$sql_query_login="SELECT * FROM employee where username='$username' AND password='$password'";
-	$result1=mysqli_query($db_link,$sql_query_login) or die("查詢失敗");
-    
+	// require("conn_mysql.php");
+	// $sql_query_login="SELECT * FROM employee where username='$username' AND password='$password'";
+	// $result1=mysqli_query($db_link,$sql_query_login) or die("查詢失敗");
+    if($username=="test" && $password="1234"){
+
+		require("to_mysql_use.php");
+	}
+	else{echo "<link href='https://www.google.com/'>";}
 	
-	require("to_mysql_use.php");
 ?>
 
 <button onclick="document.location.href='./'">登出</button>;
